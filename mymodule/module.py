32a0577df5137ee5098b214577a4c518fb8e65bc
Tarun Gaba <tarun.gaba7@gmail.com>
2018-02-25 11:07:31 +0530

    Update module.py

--- mymodule/module.py
+++ mymodule/module.py
@@ -2,9 +2,6 @@ from scipy.cluster.hierarchy import linkage
 from scipy.spatial.distance import pdist
 from sklearn.datasets import make_blobs
 
-from due import due, Doi
-
-@due.dcite(Doi("1.2.3/x.y.z"), description="Iski Bhains ka patandar")
 def my_function():
 	print("I: Simulating 4 blobs")
 	data, true_label = make_blobs(centers=4)
