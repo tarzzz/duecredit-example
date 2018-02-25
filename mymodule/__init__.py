880abe5a3fe7f23f602e8febf156df97a1b8dc39
Tarun Gaba <tarun.gaba7@gmail.com>
2018-02-25 11:09:45 +0530

    Update __init__.py

--- mymodule/__init__.py
+++ mymodule/__init__.py
@@ -1,4 +1,8 @@
-from module import my_function
 from due import due, Doi
 
 due.cite(Doi("1.2.3/x.y.z"), description="Iski Bhains ka patandar")
+
+from module import my_function
+from M1.module import my_function1
+from M2.module import my_function1
+
